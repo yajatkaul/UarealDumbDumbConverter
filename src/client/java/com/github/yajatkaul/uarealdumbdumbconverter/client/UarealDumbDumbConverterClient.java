6b4c2bf9aa931414da659ba69113d1027f5a20d3
yajatkaul <mc.crafter1973@gmail.com>
2026-01-17 17:34:08 +0530

    Complete

--- src/client/java/com/github/yajatkaul/uarealdumbdumbconverter/client/UarealDumbDumbConverterClient.java
+++ src/client/java/com/github/yajatkaul/uarealdumbdumbconverter/client/UarealDumbDumbConverterClient.java
@@ -3,8 +3,6 @@ package com.github.yajatkaul.uarealdumbdumbconverter.client;
 import net.fabricmc.api.ClientModInitializer;
 
 public class UarealDumbDumbConverterClient implements ClientModInitializer {
-
-
     @Override
     public void onInitializeClient() {
     }
